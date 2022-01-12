ee43c660f9abf1d77c48f269ce3ebb8a38b6c42e
gokulnath17 <gokulnath@thirvusoft.in>
2022-01-12 13:26:54 +0530

    Testing

---
+++ glenmargon_backend/glenmargon_backend/doctype/aa/aa.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, glenmargon_backend and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('aa', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ glenmargon_backend/glenmargon_backend/doctype/aa/aa.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, glenmargon_backend and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class aa(Document):
+	pass
