b8024726fd09a6aea631ab9e22dca31d8ca29338
Gokulnath <95605271+Gokulnath17@users.noreply.github.com>
2022-01-18 11:08:54 +0530

    Delete glenmargon_backend/glenmargon_backend/doctype/ts_precious_metal_bill_image directory

--- glenmargon_backend/glenmargon_backend/doctype/ts_precious_metal_bill_image/ts_precious_metal_bill_image.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, thirvusoft and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class TSPreciousMetalBillImage(Document):
-	pass
